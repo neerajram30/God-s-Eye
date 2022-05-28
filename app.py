dce415a290366a23557ee793a17b6c165e1648f7
neerajram30 <neerajramachandran30@gmail.com>
2022-05-29 02:04:19 +0530

    file

--- app.py
+++ app.py
@@ -22,43 +22,29 @@ DB_USER = "postgres"
 DB_PASS = "12345"
 
 conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST)
-
+UPLOAD_FOLDER = 'static/uploads/'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
-if(conn):
-    print("connection established")
-
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-     
+      
 
 
-def predict(img_path, knn_clf=None, model_path=None, threshold=0.6): # 6 needs 40+ accuracy, 4 needs 60+ accuracy
-    if knn_clf is None and model_path is None:
-        raise Exception("Must supply knn classifier either thourgh knn_clf or model_path")
-    # Load a trained KNN model (if one was passed in)
-    if knn_clf is None:
-        with open(model_path, 'rb') as f:
-            knn_clf = pickle.load(f)
-    # Load image file and find face locations
-    img = img_path
-    face_box = face_recognition.face_locations(img)
-    # If no faces are found in the image, return an empty result.
-    if len(face_box) == 0:
-        return []
-    # Find encodings for faces in the test iamge
-    faces_encodings = face_recognition.face_encodings(img, known_face_locations=face_box)
-    # Use the KNN model to find the best matches for the test face
-    closest_distances = knn_clf.kneighbors(faces_encodings, n_neighbors=2)
-    matches = [closest_distances[0][i][0] <= threshold for i in range(len(face_box))]
-    # Predict classes and remove classifications that aren't within the threshold
-    return [(pred, loc) if rec else ("unknown", loc) for pred, loc, rec in zip(knn_clf.predict(faces_encodings),face_box,matches
-    )]
+known_face_encodings=[]
+known_face_names=[]
+face_locations = []
+face_encodings = []
+face_names = []
+process_this_frame = True
+
+if(conn):
+    print("connection established")
+
+camera = cv2.VideoCapture(0)
 
 
-camera =cv2.VideoCapture(0)
-
-def gen_frames():
+def gen_frames():  
     while True:
         success, frame = camera.read()  # read the camera frame
         if not success:
@@ -69,41 +55,56 @@ def gen_frames():
             # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
             rgb_small_frame = small_frame[:, :, ::-1]
 
+            # Only process every other frame of video to save time
+           
+            # Find all the faces and face encodings in the current frame of video
+            face_locations = face_recognition.face_locations(rgb_small_frame)
+            face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+            face_names = []
+            for face_encoding in face_encodings:
+                # See if the face is a match for the known face(s)
+                matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+                name = "Unknown"
+                # Or instead, use the known face with the smallest distance to the new face
+                face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
+                best_match_index = np.argmin(face_distances)
+                if matches[best_match_index]:
+                    name = known_face_names[best_match_index]
 
+                face_names.append(name)
+            
 
-            small_frame=cv2.cvtColor(small_frame, cv2.COLOR_BGR2RGB)
-
-            predictions = predict(small_frame, model_path="classifier/trained_knn_model.clf")
-
-
-
-            for name, (top, right, bottom, left) in predictions:
+            # Display the results
+            for (top, right, bottom, left), name in zip(face_locations, face_names):
+                # Scale back up face locations since the frame we detected in was scaled to 1/4 size
                 top *= 4
                 right *= 4
                 bottom *= 4
                 left *= 4
-                # cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 255), 2)
+
+                # Draw a box around the face
+                cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+
+                # Draw a label with a name below the face
                 cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
                 font = cv2.FONT_HERSHEY_DUPLEX
-                #cv2.putText(frame, name, (left-10,top-6), font, 0.8, (255, 255, 255), 1)
                 cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+
             ret, buffer = cv2.imencode('.jpg', frame)
             frame = buffer.tobytes()
             yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n') 
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 
-
-           
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 
-@app.route('/video_feed')
-def video_feed():
-    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+# @app.route('/video_feed')
+# def video_feed():
+#     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
 @app.route('/newcase')
@@ -115,7 +116,7 @@ def newcase_upload():
     m_name = request.form.get("m_name","")
     mobile = request.form.get("mobile","")
     location = request.form.get("location","")
-    face_img = request.form.get("face_img","")
+    face_img = request.files['face_img']
 
     if m_name == "":
         msg = "Please fill in name"
@@ -130,14 +131,18 @@ def newcase_upload():
     if face_img == "":
         msg = "Please upload in face image"
         return render_template("newcase.html", message = msg)
+    if face_img and allowed_file(face_img.filename):
+        filename = secure_filename(face_img.filename)
+        face_img.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))    
     cur = conn.cursor()
-    cur.execute(f"INSERT INTO cases (m_name,mobile,location,face_img) VALUES('{m_name}','{mobile}','{location}','{face_img}')")
+    cur.execute("INSERT INTO cases (m_name, mobile, location, face_img) VALUES('{m_name}', '{mobile}', '{location}', '{face_img}')")
     try:
         conn.commit()
     except psycopg2.Error as e:
         msg="Database error: " + e
         return render_template("newcase.html", message = msg)
-    cur.close()        
+    cur.close()
+    return redirect('/')        
 
 
     
@@ -155,15 +160,15 @@ def train():
         return render_template("newcase.html", message = msg)
     results =cur.fetchall()
     for i in results:
-         name = i[0]
-         image = i[1]
-         print(name+image)   
+        name = i[0]
+        image = i[1]
+        face_image = face_recognition.load_image_file(image)
+        face_encoding = face_recognition.face_encodings(face_image)[0]
+
+        known_face_encodings.append(face_encoding)
+        known_face_names.append(name) 
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')        
     cur.close()        
-    
-
-
-
-    return "some"
     
 if __name__=='__main__':
     app.run(debug=True)
